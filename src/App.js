c6cf64de69e7eefe9b0e96c658b65319b9949e38
Opeyemi Akinnawo <opeyemi.akinnawo@interswitchgroup.com>
2017-05-22 18:54:58 +0100

    code base for index component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router'
 import './App.css';
+
+import AuthService from './util/AuthService'
+
+//import page components...
+import Dashboard from './pages/dashboard/dashboard';
+import Login from './login/login';
+import Home from './pages/home/home';
+import Profile from './pages/profile/profile';
+
+const auth = new AuthService();
+
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' })
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={browserHistory}>
+          <Route path="/">
+            <IndexRedirect to="/home" />
+            <Route path="login" component={() => (<Login auth={auth}/>)} />
+            <Route component={Dashboard} auth={auth} onEnter={requireAuth}>
+              <Route path="home" component={Home} />
+              <Route path="profile" component={Profile} />
+            </Route>
+          </Route>
+      </Router>
     );
   }
 }
