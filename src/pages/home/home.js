957205beba2702addb50a70aee9f2b20e12fe50f
Opeyemi Akinnawo <opeyemi.akinnawo@interswitchgroup.com>
2017-05-22 19:40:34 +0100

    sample home component built

---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="wrapper wrapper-content">
+                Welcome...
+            </div>
+        );
+    }
+}
+
+export default Home;
