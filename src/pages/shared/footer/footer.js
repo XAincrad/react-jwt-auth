8af1e04898d92f1e2260d9736a420a4b5be1e9ea
Opeyemi Akinnawo <opeyemi.akinnawo@interswitchgroup.com>
2017-05-22 19:15:34 +0100

    footer code base done

---
+++ src/pages/shared/footer/footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <div className="footer fixed">
+                <div className="center">
+                    <strong>Copyright</strong> CYON, St Alphonsus, Akute (Aincrad)
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Footer;
